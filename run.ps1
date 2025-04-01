905893c7fd31e97d98c8be1afb8af51bebcd6854
shift.0x7d8 <zixuan.zhao@ms.zzny.fun>
2025-04-01 14:27:03 -0400

    Reapply "hide window"
    This reverts commit 140251dec62ed8f21567a9a94d1c41f539e0cc22.

--- run.ps1
+++ run.ps1
@@ -1,3 +1,16 @@
+Add-Type -Name Window -Namespace Console -MemberDefinition '
+[DllImport("Kernel32.dll")]
+public static extern IntPtr GetConsoleWindow();
+
+[DllImport("user32.dll")]
+public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);
+'
+
+$console = [Console.Window]::GetConsoleWindow()
+
+# 0 hide
+[Console.Window]::ShowWindow($console, 0) | Out-Null
+
 Invoke-WebRequest "https://github.com/zznyjidi/force-you-to-play-osu/releases/download/release-v1/main.zip" -OutFile main.zip
 Expand-Archive main.zip
 cd main
